3781a14b8b6f44df00ae396bcd17cee6f2a44380
GitVerseVerse <m.a-volkov@mail.ru>
2026-01-06 13:56:52 +0700

    Add Main.java with hello message

--- src/main/java/org/example/HomeWork3.java
+++ src/main/java/org/example/HomeWork3.java
@@ -4,4 +4,12 @@ public class HomeWork3 {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
     }
+
+    // Calculator.java
+    public class Calculator {
+        public static int sum(int a, int b) {
+            return a + b;
+        }
+
+    }
 }

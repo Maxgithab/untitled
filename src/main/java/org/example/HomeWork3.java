22912e8ecdd78fd32e2dd045396c54fd14e04d6d
GitVerseVerse <m.a-volkov@mail.ru>
2026-01-06 13:59:59 +0700

    Update Main to use Calculator

--- src/main/java/org/example/HomeWork3.java
+++ src/main/java/org/example/HomeWork3.java
@@ -12,4 +12,10 @@ public class HomeWork3 {
         }
 
     }
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello Git!");
+            System.out.println("Sum: " + Calculator.sum(5, 3));
+        }
+    }
 }

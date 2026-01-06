3bc55dfbbea83e5160fb44bdbaf622660c53d6a0
GitVerseVerse <m.a-volkov@mail.ru>
2026-01-06 13:55:02 +0700

    Add Main.java with hello message

--- src/main/java/org/example/HomeWork3.java
+++ src/main/java/org/example/HomeWork3.java
@@ -1,9 +1,7 @@
 package org.example;
 
 public class HomeWork3 {
-    public class Main {
-        public static void main(String[] args) {
-            System.out.println("Hello Git!");
-        }
-
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
 }

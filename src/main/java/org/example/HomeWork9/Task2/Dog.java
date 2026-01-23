1d1617f3da2b1be97957f8c45779f08c89e31b25
GitVerseVerse <m.a-volkov@mail.ru>
2026-01-23 11:59:10 +0700

    HomeWork 9, Task 1, 2(step 1,2, check)

--- src/main/java/org/example/HomeWork9/Task2/Dog.java
+++ src/main/java/org/example/HomeWork9/Task2/Dog.java
@@ -1,11 +1,25 @@
 package org.example.HomeWork9.Task2;
 
 public class Dog {
-    public Dog() {
+
+    private static Dog instance;
+
+    private Dog() {
         System.out.println("Один собака");
+    }
+
+    public static Dog getInstance() {
+        if (instance == null) {
+            instance = new Dog();
+        }
+        return instance;
     }
 
     public void voice() {
         System.out.println("Ауф!");
+    }
+
+    public void eat() {
+        System.out.println("Собака ест");
     }
 }
--- src/main/java/org/example/HomeWork9/Task2/Main.java
+++ src/main/java/org/example/HomeWork9/Task2/Main.java
@@ -1,8 +1,34 @@
 package org.example.HomeWork9.Task2;
 
+/*Написать такой конструктор, который запретит создание объекта класса Dog в других
+классах; Найти способ вызывать методы voice(), eat() класса Dog, который позволит
+обойти ограничение, наложенное конструктором выше. Объяснить, как работают оба
+способа.*/
+
+import java.lang.reflect.Constructor;
+
 public class Main {
-    public static void main(String[] args) {
-        Dog dog = new Dog();
+    public static void main(String[] args) throws Exception {
+        //Dog dog = new Dog(); // при Private не создать new
+        Dog dog = Dog.getInstance();
+        /* запрос через getInstance(), создание и возврат в единственном экземпляре */
         dog.voice();
+        dog.eat();
+
+        Dog dog2 = Dog.getInstance();
+        System.out.println(dog == dog2); /* dog2 и dog - это одна и та же собака*/
+        dog2.voice();
+        dog2.eat();
+
+        Constructor<Dog> constructor = Dog.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Dog dog3 = constructor.newInstance();
+        System.out.println(dog == dog3); /* dog3 и dog - это разные собаки*/
+        dog3.voice();
+        dog3.eat();
+/* getDeclaredConstructor() - получение доступа к приватному конструктору
+   setAccessible(true) - отключение проверки private
+   newInstance() - создание объекта через приватный конструктор
+   получаем новый объект, хотя по задумке должен быть только один */
     }
 }
